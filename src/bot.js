bc656baf85ffb47f8308ac945766a77122b7b035
Loic Correvon <pumbaa666@gmail.com>
2023-03-15 16:50:01 +0100

    use kaamelott-soundboard audio instead of pumbaa.ch

--- src/bot.js
+++ src/bot.js
@@ -32,7 +32,7 @@ logger.add(new logger.transports.Console, {
 });
 logger.level = 'debug';
 
-const baseUrl = "http://pumbaa.ch/public/kaamelott/"; // TODO kaamelott-board https://github.com/2ec0b4/kaamelott-soundboard/sounds/
+const baseUrl = "https://github.com/2ec0b4/kaamelott-soundboard/tree/master/sounds"; // a (unsynched) backup exists on http://pumbaa.ch/public/kaamelott/
 let isBotPlayingSound = false;
 
 async function start() {
